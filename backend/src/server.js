c834c4708b91d44f6bfc53e4227d2baecf1b7eb9
touaibiariad0603 <touaibiariad0603@example.com>
2026-03-10 18:11:36 +0100

    deplyment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,10 +12,10 @@ app.get("/api/health",(req,res)=>{
 });
 //make our app ready for deployment
 if(ENV.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../admin/dist")))
+    app.use(express.static(path.join(_dirname,"../admin/dist")))
 
     app.get("/{*any}",(req,res) =>{
-        res.sendFile(path.join(__dirname,"../admin","dist","index.html"));
+        res.sendFile(path.join(_dirname,"../admin","dist","index.html"));
     } );
 }
 
