ff425986577faeef0a78817018549baaa46211b9
touaibiariad0603 <touaibiariad0603@example.com>
2026-03-11 01:58:05 +0100

    routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,12 @@ const app = express();
 
 app.use(express.json());
 
+// Friendly root route
+app.get("/", (req, res) => {
+  res.send("Dabarli Backend is running!");
+});
+
+// API routes
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "success" });
 });
