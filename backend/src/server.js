c0afb2e3d0e4ed465b70dff934ab5ca3b095a25b
touaibiariad0603 <touaibiariad0603@example.com>
2026-03-11 01:53:21 +0100

    /

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,9 +17,9 @@ app.get("/api/health", (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../admin/dist")));
 
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../admin/dist/index.html"));
-  });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../admin/dist/index.html"));
+});
 }
 
 const PORT = process.env.PORT || 3000;
