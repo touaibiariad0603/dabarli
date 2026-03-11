75aa0ef336bfec2cd228255552c1a525b037a14c
touaibiariad0603 <touaibiariad0603@example.com>
2026-03-11 01:55:50 +0100

    render

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,12 @@
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
 
 const app = express();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 app.use(express.json());
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "success" });
 });
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../admin/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../admin/dist/index.html"));
-});
-}
 
 const PORT = process.env.PORT || 3000;
 
