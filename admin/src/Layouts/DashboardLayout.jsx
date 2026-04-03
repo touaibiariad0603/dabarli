236b325f69999b90f563db7b3daa1ab656ba1714
touaibiariad0603 <touaibiariad0603@gmail.com>
2026-04-03 12:28:49 +0200

    admin dashboard

--- admin/src/Layouts/DashboardLayout.jsx
+++ admin/src/Layouts/DashboardLayout.jsx
@@ -1,12 +1,21 @@
 import { Outlet } from "react-router";
+import Navbar from "../components/navbar";
+import Sidebar from "../components/sidebar";
 
 function DashboardLayout(){
     return (
-        <div>
-            <h1>sidebar</h1>
-            <h1>navbar</h1>
-            <Outlet/>
-        </div>
-    )
+        <div className="drawer lg:drawer-open">
+            <input id="my-drawer" type="checkbox" className="drawer-toggle" defaultChecked />   
+
+            <div className="drawer-content">
+                <Navbar />
+
+                <main className="p-6">
+                    <Outlet />
+                </main>
+            </div>
+        <Sidebar/>
+    </div>
+    );
 }
-export default DashboardLayout
+export default DashboardLayout;
--- admin/src/lib/axios.js
+++ admin/src/lib/axios.js
@@ -5,4 +5,4 @@ const axiosInstance = axios.create({
     withCredentials:true,//by adding this field browser will send the cookies to server automatically, on every single req
 })
 
-export default axios
+export default axiosInstance
