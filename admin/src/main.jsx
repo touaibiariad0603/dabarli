36290f6fb7cc8982f81f54231efe605149a3c689
touaibiariad0603 <touaibiariad0603@gmail.com>
2026-04-06 11:40:49 +0200

    adding the customer page and sentry

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -22,7 +22,7 @@ const queryClient = new QueryClient()
 
 
 Sentry.init({
-  dsn: import.meta.VITE_SENTRY_DSN,
+  dsn: import.meta.env.VITE_SENTRY_DSN,
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
   sendDefaultPii: true,
