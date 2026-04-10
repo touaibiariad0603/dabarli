407d1901648b9fda205ea0a6b71dfa9de1f0504e
touaibiariad0603 <touaibiariad0603@gmail.com>
2026-04-10 19:35:41 +0200

    wishlist

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,37 +1,38 @@
-import {Inngest} from "inngest";
-import {connectDB} from './db.js'
-import { user } from "../models/user.model.js";
+import { Inngest } from "inngest";
+import { connectDB } from "./db.js";
+import { User } from "../models/user.model.js";
 
-export const inngest = new Inngest({id:"dabarli-app"})
+export const inngest = new Inngest({ id: "ecommerce-app" });
 
 const syncUser = inngest.createFunction(
-    {id:"sync-user"},
-    {event:"clerk/user.created"},
-    async({event})=>{
-        await connectDB();
-        const {id, email_addresses, first_name, last_name, image_url} = event.data;
-        
-        const newUser = {
-            clerkId: id,                                          
-            email: email_addresses[0]?.email_address,            
-            name: `${first_name || ""} ${last_name || ""}`.trim(), 
-            imageUrl: image_url,                                 
-            addresses: [],
-            wishlist: []                                          
-        };
+  { id: "sync-user" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    await connectDB();
+    const { id, email_addresses, first_name, last_name, image_url } = event.data;
 
-        await user.create(newUser);
-    }
+    const newUser = {
+      clerkId: id,
+      email: email_addresses[0]?.email_address,
+      name: `${first_name || ""} ${last_name || ""}` || "User",
+      imageUrl: image_url,
+      addresses: [],
+      wishlist: [],
+    };
+
+    await User.create(newUser);
+  }
 );
 
 const deleteUserFromDB = inngest.createFunction(
-    {id:"delete-user-from-dv"},
-    {event:"clerk/user.deleted"},
-    async ({event})=>{
-        await connectDB();
-        const {id} = event.data;
-        await user.deleteOne({clerkId: id});  
-    }
+  { id: "delete-user-from-db" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    await connectDB();
+
+    const { id } = event.data;
+    await User.deleteOne({ clerkId: id });
+  }
 );
 
-export const functions = [syncUser, deleteUserFromDB]
+export const functions = [syncUser, deleteUserFromDB];
--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,39 +1,35 @@
 import { requireAuth } from "@clerk/express";
-import {user } from "../models/user.model.js";
+import { User } from "../models/user.model.js";
 import { ENV } from "../config/env.js";
 
-export const protectRoute=[
-    requireAuth(),
-    async (req,res,next)=>{
-        try{
-            const clerkId = req.auth().userId
-            if(!clerkId) return res.status(401).json({message:"unauthorized - invalid token"});
+export const protectRoute = [
+  requireAuth(),
+  async (req, res, next) => {
+    try {
+      const clerkId = req.auth().userId;
+      if (!clerkId) return res.status(401).json({ message: "Unauthorized - invalid token" });
 
-            const dbuser = await user.findOne({clerkId})
-            if(!dbuser) return res.status(404).json({message:"user not found"});
-            
-            req.user=dbuser     
-            
-            next()
-                
-        }catch(error){
-            console.error("error in protectRoute middleware",error);
-            res.status(500).json({message:"internal sercer error"});
-            
+      const user = await User.findOne({ clerkId });
+      if (!user) return res.status(404).json({ message: "User not found" });
 
-        }
-    },
+      req.user = user;
 
+      next();
+    } catch (error) {
+      console.error("Error in protectRoute middleware", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  },
 ];
 
-export const adminOnly=(req,res,next)=>{
-    if(!req.user){
-        return res.status(401).json({message:"Unauthorized - user not found"})
-    }
+export const adminOnly = (req, res, next) => {
+  if (!req.user) {
+    return res.status(401).json({ message: "Unauthorized - user not found" });
+  }
 
-    if (req.user.email !==ENV.ADMIN_EMAIL){
-        return res.status(403).json({message:'Forbidden - admin acces only'})
-    }
-    
-    next();
-}   
+  if (req.user.email !== ENV.ADMIN_EMAIL) {
+    return res.status(403).json({ message: "Forbidden - admin access only" });
+  }
+
+  next();
+};
