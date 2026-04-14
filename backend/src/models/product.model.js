da22972b6191702cc7af0ef4c2a170dc3d313410
touaibiariad0603 <touaibiariad0603@gmail.com>
2026-04-14 14:31:40 +0200

    fixes in mongo

--- backend/src/models/product.model.js
+++ backend/src/models/product.model.js
@@ -1,38 +1,48 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const productSchema =new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-        description:{
-        type:String,
-        required:true
+    description: {
+      type: String,
+      required: true,
     },
-        price:{
-        type:Number,
-        required:true,
-        min:0,
-        default:0,
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
     },
-        category:{
-        type:String,
-        required:true
+    stock: {
+      type: Number,
+      required: true,
+      min: 0,
+      default: 0,
     },
-        images:{
-        type:[String],
-        required:true
-        
+    category: {
+      type: String,
+      required: true,
     },
-        averageRating:{
-        type:Number,
-        min:0,
-        max:5,
-        default:0
+    images: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    averageRating: {
+      type: Number,
+      min: 0,
+      max: 5,
+      default: 0,
     },
-        totalReviews:{
-        type:Number,
-        default:0
+    totalReviews: {
+      type: Number,
+      default: 0,
     },
-},{timestamps:true});
-export const Product = mongoose.model("Product",productSchema);
+  },
+  { timestamps: true }
+);
+
+export const Product = mongoose.model("Product", productSchema);
