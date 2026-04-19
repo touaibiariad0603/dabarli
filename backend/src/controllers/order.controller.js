effb9298bb076db0de3219ccdc3a10551fe83bba
touaibiariad0603 <touaibiariad0603@gmail.com>
2026-04-19 17:53:41 +0200

    order

--- backend/src/controllers/order.controller.js
+++ backend/src/controllers/order.controller.js
@@ -11,7 +11,6 @@ export async function createOrders(req, res) {
       return res.status(400).json({ error: "No order items" });
     }
 
-  
     for (const item of orderItems) {
       const product = await Product.findById(item.product._id);
       if (!product) {
@@ -22,7 +21,7 @@ export async function createOrders(req, res) {
       }
     }
 
-
+   
     const mappedOrderItems = orderItems.map((item) => ({
       Product:  item.product._id,
       name:     item.product.name,
@@ -34,7 +33,7 @@ export async function createOrders(req, res) {
     const order = await Order.create({
       user:            user._id,
       clerkId:         user.clerkId,
-      orderItems:      mappedOrderItems,
+      orderItems:      mappedOrderItems, 
       shippingAddress,
       paymentResult,
       totalPrice,
@@ -50,7 +49,35 @@ export async function createOrders(req, res) {
     res.status(201).json({ message: "Order created successfully", order });
 
   } catch (error) {
-    console.error("Error in createOrder controller", error.message); 
+    console.error("Error in createOrder controller", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
+}
+export async function getUserOrders(req,res) {
+    try {
+        const orders = await Order.find({clerkId:req.user.clerkId})
+        .populate("orderItems.product")
+        .sort({createdAt:-1});
+
+        //check if each order has been reviewd
+        const orderIds = orders.map((order)=>order._id);
+        const reviews = await Review.find({orderId:{$in:orderIds}});
+        const reviewOrderIds = new Set(reviews.map((review)=>review.orderId.toString()));
+
+
+
+        const ordersWithReviewStatus = await Promise.all(
+            orders.map(async(order)=>{
+                return{
+                ...order.toObject(),
+                hasReviewed:reviewOrderIds.has(order._id.toString()),
+                };
+            })
+        );
+
+        res.status(200).json({orders:ordersWithReviewStatus});
+    } catch (error) {
+        console.error("Error in  getAllOrders controller",error);
+        res.status(500).json({error:"Internal server error"});
+    }
 }
