5919718fcd817d0649b3a620f13c10330daeeb6b
Abdou se <abdouselim.edu@gmail.com>
2026-05-19 16:19:28 +0100

    updqte

--- admin/src/pages/DashboardPage.jsx
+++ admin/src/pages/DashboardPage.jsx
@@ -7,6 +7,7 @@ import {
   UsersIcon,
 } from "lucide-react";
 import { capitalizeText, formatDate, getOrderStatusBadge } from "../lib/utils";
+import { formatPrice } from "../components/format-price";
 
 function DashboardPage() {
   const { data: ordersData, isLoading: ordersLoading } = useQuery({
@@ -26,7 +27,7 @@ function DashboardPage() {
       name: "Total Revenue",
       value: statsLoading
         ? "..."
-        : `${statsData?.totalRevenue?.toFixed(2) || 0}dz`,
+        : formatPrice(statsData?.totalRevenue?.toFixed(2) || 0),
       icon: <DollarSignIcon className="size-8" />,
     },
     {
