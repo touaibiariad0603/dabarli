5031fcd02d1d934eeda75bda0c049febddffda56
Abdou se <abdouselim.edu@gmail.com>
2026-05-19 16:34:54 +0100

    fix price

--- admin/src/pages/DashboardPage.jsx
+++ admin/src/pages/DashboardPage.jsx
@@ -25,7 +25,7 @@ function DashboardPage() {
   const statsCards = [
     {
       name: "Total Revenue",
-      value: statsLoading ? "..." : formatPrice(statsData?.totalRevenue || 0),
+      value: statsLoading ? "..." : statsData?.totalRevenue || 0,
       icon: <DollarSignIcon className="size-8" />,
     },
     {
